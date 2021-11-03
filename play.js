b5677bc502c076250d3351100f7d899730749845
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-03 16:39:06 -0400

    setting up stdin

--- play.js
+++ play.js
@@ -1,5 +1,18 @@
-const net = require("net");
 const {connect} = require('./client');
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (input)=>{
+    if (input === '\u0003') {
+      console.log();
+      process.exit();
+    }
+  });
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
