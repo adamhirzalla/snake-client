01ab80cee4d9420ddaef3d1151fc0cb43a61695b
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-03 16:43:17 -0400

    adding input.js

--- play.js
+++ play.js
@@ -1,17 +1,5 @@
 const {connect} = require('./client');
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (input)=>{
-    if (input === '\u0003') {
-      console.log();
-      process.exit();
-    }
-  });
-  return stdin;
-};
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
